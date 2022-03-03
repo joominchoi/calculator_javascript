8e727074ddc67f5b77c9d92a20604ede1c7ef839
JooMin Choi <joominchoi23@gmail.com>
2022-03-03 22:10:52 +0000

    created constant variables so all elements are selected

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-number')
+const operationButtons = document.querySelectorAll('[data-operation')
+const equalsButtons = document.querySelectorAll('[data-equals')
+const deleteButtons = document.querySelectorAll('[data-delete')
+const allClearButtons = document.querySelectorAll('[data-all-clear')
+const previousOperandTextElement = document.querySelector('[data-previous-operand')
+const currentOperandTextElement = document.querySelector('[data-current-operand')
